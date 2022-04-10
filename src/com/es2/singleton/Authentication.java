523135a7baff02c993b75b0b6a483be6c80259cf
JoelGoncalves <joelkunguromix@gmail.com>
2022-04-10 12:19:58 +0100

    edit singleton implementation, with tests in main

--- src/com/es2/singleton/Authentication.java
+++ src/com/es2/singleton/Authentication.java
@@ -4,43 +4,55 @@ import java.util.HashMap;
 
 public class Authentication extends Object {
 
-    private HashMap<String, String> data = new HashMap<>();
-    private static Authentication Instance=null;
+    // private String ConnectionString;
+    private static Authentication	Instance=null;
+    private String Path;
+    private HashMap<String,String> hashUsers=new HashMap<>();
 
     private Authentication() {
     }
-
-    public static Authentication getInstance(){
-        if(Instance == null){
-            Instance = new Authentication();
+    /*
+        public String getConnectionString() {
+            return ConnectionString;
+        }
+        public void setConnectionString(String connectionString) {
+            this.ConnectionString = connectionString;
+        }
+    */
+    public static Authentication getInstance() {
+        if(Instance==null){//Se vazio
+            Instance = new Authentication();//cria nova
+            System.out.println("Nova ligação BD");
         }
         return Instance;
     }
-
-    public void addUser(String nome, String pw){
-        if(data.containsKey(nome)){
-            System.out.println("User already exists!");
-        }
-        else
-            data.put(nome,pw);
+    public String getPath() {
+        return Path;
+    }
+    public void setPath(String path) {
+        this.Path = path;
+        System.out.println("string ligação: "+path);
     }
 
-    public void show(){
-            System.out.println(data);
-    }
-
-    public void login(String nome, String pw){
-        if(!data.containsKey(nome)){
-            System.out.println("User not registered!");
-        }
+    public void addUser(String name, String password) {
+        if(hashUsers.containsKey(name))
+            System.out.println("Utilizador já existe!");
         else
         {
-            if(data.get(nome) == pw)
-                System.out.println(nome+" Logged in!");
-            else
-                System.out.println("Wrong password!");
+            hashUsers.put(name, password);
+            System.out.println("Novo utilizador inserido!");
         }
     }
 
-
+    public void login(String name,String password){
+        if(!hashUsers.containsKey(name))
+            System.out.println("Utilizador não registado");
+        else
+        {
+            if(hashUsers.get(name)==password)
+                System.out.println("Login com sucesso");
+            else
+                System.out.println("Password errada");
+        }
+    }
 }
